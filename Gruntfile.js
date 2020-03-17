d52366d452e6a827fb94644eeee730e3e18f00c8
wangshengying <1600700160@qq.com>
2020-03-17 20:48:26 +0800

    three

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,32 +1,34 @@
 /* global module: true */
 module.exports = function (grunt) {
-    grunt.initConfig({
-      htmlmin: {
-        options: {
-          collapseWhitespace: true,
-          preserveLineBreaks: false
-        },
+  grunt.initConfig({
+    htmlmin: {
+      options: {
+        collapseWhitespace: true
+      },
+      files: {
+        src: './index.html',
+        dest: 'dist/index.html'
+      }
+    },
+    cssmin: {
+      'dist/mystyle.css': 'mystyle.css'
+    },
+    uglify: {
+      release:{
         files: {
-          src: './index.html',
-          dest: 'dist/index.html'
+          'dist/my.js': 'my.js'
         }
-      },
-      cssmin: {
-        'dist/rectangle.css': 'rectangle.css'
-      },
-      uglify: {
-        release:{
-          files: {
-            'dist/rectangle.js': 'rectangle.js',
-            'dist/calc.js': 'calc.js'
-          }
-        }
-      },
-    });
-  
-    grunt.loadNpmTasks('grunt-contrib-htmlmin');
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-  
-    grunt.registerTask('minify', ['htmlmin', 'cssmin', 'uglify']);
-  };
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
+
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks("grunt-contrib-copy");
+
+  grunt.registerTask('minify', ['htmlmin', 'cssmin', 'uglify']);
+  grunt.registerTask("release", ['copy', 'htmlmin','cssmin', 'uglify']);
+};
